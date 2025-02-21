4a0052d6b3e3a41814984fd9914e16bcbb4c0fc8
Haris Amin <haris.amin_PA@hra.nhs.uk>
2025-02-21 09:56:23 +0000

    RSP-2538: Add roles seed data

---
+++ src/Infrastructure/Rsp.UsersService.Infrastructure/SeedData/UserData.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Rsp.UsersService.Infrastructure.SeedData;
+
+internal static class UserData
+{
+    public static IList<IdentityRole> SeedRoles()
+    {
+        return SeedHelper.SeedData<IdentityRole>("Roles.json");
+    }
+}
