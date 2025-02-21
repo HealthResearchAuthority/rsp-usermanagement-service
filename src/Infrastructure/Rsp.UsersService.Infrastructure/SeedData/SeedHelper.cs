bce849ca619be5d64e6e0dd95e3cf0e9f4437691
Haris Amin <haris.amin_PA@hra.nhs.uk>
2025-02-21 09:56:23 +0000

    RSP-2538: Add roles seed data

---
+++ src/Infrastructure/Rsp.UsersService.Infrastructure/SeedData/SeedHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace Rsp.UsersService.Infrastructure.SeedData;
+
+public static class SeedHelper
+{
+    public static List<TEntity> SeedData<TEntity>(string fileName)
+    {
+        var currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        const string path = "Data";
+        var fullPath = Path.Combine(currentDirectory, path, fileName);
+
+        var result = new List<TEntity>();
+        using (var reader = new StreamReader(fullPath))
+        {
+            string json = reader.ReadToEnd();
+            result = JsonSerializer.Deserialize<List<TEntity>>(json);
+        }
+
+        return result!;
+    }
+}
