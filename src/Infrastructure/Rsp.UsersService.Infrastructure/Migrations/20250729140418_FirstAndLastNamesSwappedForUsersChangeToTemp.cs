c930ff1379a8557739017dd6788c3c67746a40cc
MateuszBoreckiHRA <mateusz.borecki_pa@hra.nhs.uk>
2025-07-30 12:26:33 +0200

    Changed FamilyName and GivenName column names

---
+++ src/Infrastructure/Rsp.UsersService.Infrastructure/Migrations/20250729140418_FirstAndLastNamesSwappedForUsersChangeToTemp.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rsp.UsersService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstAndLastNamesSwappedForUsersChangeToTemp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GivenName",
+                table: "Users",
+                newName: "FamilyNameTemp");
+
+            migrationBuilder.RenameColumn(
+                name: "FamilyName",
+                table: "Users",
+                newName: "GivenNameTemp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FamilyNameTemp",
+                table: "Users",
+                newName: "GivenName");
+
+            migrationBuilder.RenameColumn(
+                name: "GivenNameTemp",
+                table: "Users",
+                newName: "FamilyName");
+        }
+    }
+}
