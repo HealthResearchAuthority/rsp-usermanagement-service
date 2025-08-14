1b610774fac657f6f9bfff941ef457319530465d
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-08-14 11:55:03 +0100

    RSP-4390 Update to remove roles not in use

--- src/Infrastructure/Rsp.UsersService.Infrastructure/Migrations/20250814105246_UpdatesRoles.Designer.cs
+++ src/Infrastructure/Rsp.UsersService.Infrastructure/Migrations/20250814105246_UpdatesRoles.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Rsp.UsersService.Infrastructure;
 
@@ -11,9 +12,11 @@ using Rsp.UsersService.Infrastructure;
 namespace Rsp.UsersService.Infrastructure.Migrations
 {
     [DbContext(typeof(IrasIdentityDbContext))]
-    partial class IrasIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250814105246_UpdatesRoles")]
+    partial class UpdatesRoles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
