f5d4b761a13e5f57efe39303af50a8a68b03c22a
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-10-14 09:09:53 +0100

    RSP-5226/5233/5334/5235 Add user to sponsor organisation (#60)

---
+++ src/Infrastructure/Rsp.UsersService.Infrastructure/Migrations/20251013162022_AddSponsorRole.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rsp.UsersService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSponsorRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c2c2b6c9-1f6c-4a0a-9d2a-6f2b7b4b1c8e", "7f3d1a2b-5e64-4f08-9b7a-3c6d2c4b8e9f", "sponsor", "SPONSOR" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: "c2c2b6c9-1f6c-4a0a-9d2a-6f2b7b4b1c8e");
+        }
+    }
+}
